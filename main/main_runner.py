8e0083e2256a047a645f81d415df1cb9c1419e6c
Marko <markop@mailbox.org>
2020-04-30 15:17:03 +0200

    Updated the search_archive.py a  bit
    Signed-off-by: Mrk <markop@mailbox.org>

--- main/main_runner.py
+++ main/main_runner.py
@@ -1,5 +1,3 @@
-from main import imdb_crawl, search_archive
-
 # TODO - replace all text input with a GUI
 
 funcs = {
@@ -11,40 +9,35 @@ funcs = {
 affirmative = ['yes', 'y', 'yeah', 'yup', 'sure']
 
 run = input(
-    'What do you want to do?\n1.Add a movie manually\n2.Browse the local archives\n3.Search for a movie online\n\nYour choice: ')
+    'What do you want to do?\n\n\t1.Add a movie manually\n\t2.Browse the local archives\n\t3.Search for a movie online\n\n>>Your choice: ').lower()
 
-for function in funcs:
-    if run in function:
-        pass
-
-if run in funcs['add']:
+if run == '1':
 
     def movie_add():
         if input('Do you wish to view the instructions y/n? ') in affirmative:
-            # print('First option is to input the information in the following order, leaving out any deatils you '
-            #       'don\'t have')
-            # print('')
             print('name=The Matrix, director=Wachowskis, year=1994')
 
 
     movie_add()
 
-elif run in funcs['view']:
+elif run == '2':
+    from main.search_archive import search_input_handler
+
 
     def archive_browse():
-        search_input = input('What will you be searching by?\n1. Title\n2. Year\n3. Director')
-        search_archive.csv_reader(search_input)
+        search_input_handler()
+
 
     archive_browse()
 
+elif run == '3':
+    from main.imdb_crawl import dict_to_csv, imdb_search
 
-elif run in funcs['search']:
 
     def online_search():
-
-        result = imdb_crawl.imdb_search(input('Please enter the movie title you are searching for: '))
+        result = imdb_search(input('Please enter the movie title you are searching for: '))
         if result:
-            imdb_crawl.dict_to_csv(result)
+            dict_to_csv(result)
 
 
     online_search()
--- main/search_archive.py
+++ main/search_archive.py
@@ -1,19 +1,70 @@
-import os
+def search_input_handler():
+    search_input = input(
+        'Please enter the option number\n\n\t1. Title\n\t2. Year\n\t3. Director\n\t4. Display all results\n\n>> Your choice: ').lower()
+
+    if search_input == '1':
+        title = input('Please enter the movie title: ')
+        csv_reader(1, value=title)
+    elif search_input == '2':
+        year = input('Please enter the year: ')
+        csv_reader(2, value=year)
+    elif search_input == '3':
+        director = input('Please enter the director: ')
+        csv_reader(3, )
+    elif search_input == '4':
+        csv_reader(4)
+    elif search_input == 'q':
+        pass
+    else:
+        print(
+            'No such input please choose a value and enter only the singular digit next to the given option: 1,2,3,4 or q to quit')
+        search_input_handler()
 
 
-def csv_reader(search_input):
+def csv_reader(search_input, value=''):
+    """
+    :int search_input:
+    :str value:
+    :return:
+    """
+    import os
     if not os.path.exists('Results/test_results.csv'):
         # TODO - add a way to communicate with the caller
         print('The local archive does not exist, would you like to add a movie or browse for it online?')
         return 'something'
     else:
         import csv
-
         with open('Results/test_results.csv', 'r+') as movie_archive:
-
             csv_reader = csv.reader(movie_archive)
-            for count, line in enumerate(csv_reader):
-                if count == 0:
-                    keys = line
-                else:
-                    print(dict(zip(keys, line)))
+
+            if search_input == 1:
+                for count, line in enumerate(csv_reader):
+                    if count == 0:
+                        keys = line
+                    elif value in line[0].lower():
+                        print(line)
+
+            elif search_input == 2:
+                for count, line in enumerate(csv_reader):
+                    if count == 0:
+                        keys = line
+                    elif value in line[1].lower():
+                        print(line)
+
+            elif search_input == 3:
+                for count, line in enumerate(csv_reader):
+                    if count == 0:
+                        keys = line
+                    elif value in line[2].lower():
+                        print(line)
+
+            elif search_input == 4:
+                for count, line in enumerate(csv_reader):
+                    if count == 0:
+                        keys = line
+                    else:
+                        print(dict(zip(keys, line)))
+
+
+if __name__ == '__main__':
+    search_input_handler()
