87cb4ae2cc0e239c2aec3394d48cccda2394f47b
white-diavol <markop@mailbox.org>
2020-05-02 11:18:24 +0200

    main_runner.py is encapsulated, manual_add.py slight additions

--- main/main_runner.py
+++ main/main_runner.py
@@ -1,48 +1,46 @@
 # TODO - replace all text input with a GUI
 
-# funcs = {
-#     "add": ['1', '1.', 'add', 'add a movie', 'manual', 'manually', 'movie_add'],
-#     "view": ['2', '2.', 'browse', 'browse the archive', 'archive', 'archv', 'archive_browse'],
-#     "search": ['3', '3.', 'search', 'search for a movie', 'srch', 'online', 'imdb']
-# }
 
-affirmative = ['yes', 'y', 'yeah', 'yup']
+class Runner:
 
-run = input(
-    'What do you want to do?\n\n\t1.Add or remove a movie\n\t2.Browse the local archives\n\t3.Search for a movie online\n\n>>Your choice: ').lower()
+    def __init__(self):
+        self.affirmative = ['yes', 'y', 'yeah', 'yup']
+        self.run = input(
+            'What do you want to do?\n\n\t1.Add or remove a movie\n\t2.Browse the local archives\n\t3.Search for a '
+            'movie online\n\n>>Your choice: ')
 
-if run == '1':
+    def run_logic(self):
 
-    def movie_add():
+        if self.run == '1':
 
-        from main.manual_add import manual_movie_add
+            from main.manual_add import manual_movie_add
 
-        if input('Do you wish to view the instructions y/n? ') in affirmative:
-            print(
-                'Please enter the details in this order:\n\n\tTitle,Year,Director(s)\nAll separated by commas, if you are missing ')
-            manual_movie_add()
+            if input('Do you wish to view the instructions y/n? ') in self.affirmative:
+                print(
+                    'Please enter the details in this order:\n\n\tTitle,Year,Director(s)\nAll separated by commas, '
+                    'if you are missing a velue enter a space instead')
+                manual_movie_add()
+            else:
+                manual_movie_add()
+
+        elif self.run == '2':
+            from main.search_archive import search_input_handler
+
+            search_input_handler()
+
+        elif self.run == '3':
+            from main.imdb_crawl import dict_to_csv, imdb_search
+
+            result = imdb_search(input('Please enter the movie title you are searching for: '))
+            # TODO **
+            input('Do you want to: \n\t1. Save this this to the archive\n\t2. Re-do the search?\n')
+            if result:
+                dict_to_csv(result)
+
         else:
-            manual_movie_add()
+            print('Unrecognized input try with 1, 2 or 3')
 
-    movie_add()
 
-elif run == '2':
-    from main.search_archive import search_input_handler
-
-    def archive_browse():
-        search_input_handler()
-
-    archive_browse()
-
-elif run == '3':
-    from main.imdb_crawl import dict_to_csv, imdb_search
-
-    def online_search():
-        result = imdb_search(input('Please enter the movie title you are searching for: '))
-        if result:
-            dict_to_csv(result)
-
-    online_search()
-
-else:
-    print('Unrecognized input try with 1, 2 or 3')
+if __name__ == '__main__':
+    main_runner = Runner()
+    main_runner.run_logic()
--- main/manual_add.py
+++ main/manual_add.py
@@ -1,8 +1,14 @@
 def manual_movie_add():
-    test = input('Title,Year,Director OR ,Year,Director OR ,,Director etc.\n >> ').split(',', maxsplit=2)
-    output_list = [element.lstrip() for element in test]
-    print(output_list)
+    user_movie_input = input('\nTitle,Year,Director OR ,Year,Director OR ,,Director etc.\n >> ').split(',', maxsplit=2)
+    sanitized_movie_input = [element.lstrip() for element in user_movie_input]
+    while len(sanitized_movie_input) < 3:
+        sanitized_movie_input.append('_')
+    # TODO **
+    test = {'Title': sanitized_movie_input[0], 'Year': sanitized_movie_input[1], 'Director': sanitized_movie_input[2]}
+    # with open('Results/')
+    # test.items()
 
 
 if __name__ == '__main__':
+    # while True:
     manual_movie_add()
